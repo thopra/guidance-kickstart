5e8d246467262420d34ae49bad8303eaa25454e6
thopra <tpb@onm.de>
2016-01-26 00:01:05 +0100

    added gitignore and fixed paths in index.php

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
-require_once('vendor/autoload.php');
+require_once('../vendor/autoload.php');
 $styleguide = new \Thopra\Styleguide\Styleguide();
 $styleguide->setTitle('Test Styleguide');
 
-$source = new \Thopra\Styleguide\Source\Source(dirname(__FILE__), 'default', 'Example Styles');
+$source = new \Thopra\Styleguide\Source\Source(dirname(__FILE__).'/Assets', 'default', 'Example Styles');
+//todo: set directories to default
+$source->setPartialDir('../Partials');
 $source->addResource('Assets/css/main.css');
 
 $styleguide->addSource($source);
