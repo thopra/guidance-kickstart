43b6b9c9c1d867a5d7d3d8a21cf5d045ca238495
Thomas Prangenberg <tpb@onm.de>
2016-02-03 14:54:12 +0100

    updated composer dependencies to new names

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once('../vendor/autoload.php');
-require_once('guidance/Classes/Guidance.php');
-(new \Thopra\Guidance(__DIR__.'/guidance.json'))->render();
+(new \Thopra\Guidance\Frontend(__DIR__.'/guidance.json'))->render();
 
 ?>
